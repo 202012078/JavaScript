5def6f2473aa09f900d59955f9847577b09b92a9
Mihir <76066631+202012078@users.noreply.github.com>
2021-03-14 11:20:08 +0530

    exe2

---
+++ exe2.js
@@ -0,0 +1,7 @@
+console.time('Your code took');
+console.log('Hello');
+console.log(4 + 35);
+console.log({mihir:'this', marks:100});
+console.table({mihir:'this', marks:100});
+console.timeEnd('Your code took');
+// console.assert(123<5,'This age is not possible')
