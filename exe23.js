55bdb9f68d84e63884bb4d2991878dafaeeaa791
Mihir Zalavadiya <76066631+202012078@users.noreply.github.com>
2021-03-16 10:59:53 +0530

    exe23
    Math Object Information

---
+++ exe23.js
@@ -0,0 +1,30 @@
+console.log(`This is a tutorial 23. This tutorial describe a Math Object.`);
+
+let x = 3;
+let y = 6;
+let z;
+
+z = x + y;
+z = x - y;
+z = x * y;
+z = x / y;
+console.log(z);
+
+/* Exploaring the Math Object */
+
+z = Math;
+z = Math.PI;
+z = MAth.E;
+z = Math.round(5.4);
+z = Math.ceil(5.3);
+z = Math.floor(-5.3);
+z = Math.abs(-3);
+z = Math.sqrt(4);
+z = Math.min(1, 2, 3, 4, 34, 45, 999, 0);
+z = Math.max(1, 2, 3, 4, 34, 45, 999, 0);
+z = Math.random();
+z = 100 * Math.random();
+z = 50 + (100 - 50) * Math.random();
+z = Math.ceil(50 + (100 - 50) * Math.random());
+
+console.log(z);
